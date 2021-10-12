ceb2bc055ae887682f9a8ab180cf903e482d3759
Mukul Jain <mukuljain422@gmail.com>
2021-10-12 20:45:46 +0530

    check option for ladder, no play and snake

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -8,5 +8,20 @@ public class SnakeNLadder {
 
 		// Random Dice value between 1-6
 		int dice_value = (int) Math.floor((Math.random()*6) + 1);
+		
+		// Random Option for No play, Ladder, and Snake
+		int check_opt = (int) Math.floor(Math.random()*3);
+		
+		//checking option using case
+		switch(check_opt) {
+			case 1:		//ladder
+				start_pos += dice_value;
+				break;
+			case 2:		//snake
+				start_pos -= dice_value;
+				break;
+			default:	//no play
+				start_pos += 0;
+		}
 	}
 }
