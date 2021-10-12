d6307e120ebdca6d93b6121f119bb0d0e64cacd0
Mukul Jain <mukuljain422@gmail.com>
2021-10-12 21:57:48 +0530

    add loop which run till final position

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -5,6 +5,7 @@ public class SnakeNLadder {
 		
 		//variables
 		int positionPlayer1 = 0;
+		int finalPosition = 100;
 
 		// Random Dice value between 1-6
 		int dice_value = (int) Math.floor((Math.random()*6) + 1);
@@ -12,16 +13,21 @@ public class SnakeNLadder {
 		// Random Option for No play, Ladder, and Snake
 		int check_opt = (int) Math.floor(Math.random()*3);
 		
-		//checking option using case
-		switch(check_opt) {
-			case 1:		//ladder
-				start_pos += dice_value;
-				break;
-			case 2:		//snake
-				start_pos -= dice_value;
-				break;
-			default:	//no play
-				start_pos += 0;
+		//loops execute till final position
+		while(start_pos != final_pos){
+			//checking option using case
+			switch(check_opt) {
+				case 1:		//ladder
+					positionPlayer1 += dice_value;
+					break;
+				case 2:		//snake
+					positionPlayer1 -= dice_value;
+					if(positionPlayer1 < 0)
+						positionPlayer1 = 0;
+					break;
+				default:	//no play
+					positionPlayer1 += 0;
+			}
 		}
 	}
 }
